4091463b2e99fd877b74536053051b70b8d5ab1f
Sales manager + developer <39089696+GavinMarsh@users.noreply.github.com>
2020-03-03 21:14:19 +0000

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,11 @@
 @extends('_layouts.master')
 
-//@push('meta')
+@push('meta')
     <meta property="og:title" content="{{ $page->siteName }}" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ $page->getUrl() }}"/>
     <meta property="og:description" content="{{ $page->blogDescription }}" />
-//@endpush
+@endpush
 
 @section('body')
 
