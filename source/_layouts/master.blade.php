3cc43387116b151a93ac2f2e06b2a59757339229
Sales manager + developer <39089696+GavinMarsh@users.noreply.github.com>
2020-03-03 21:57:03 +0000

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -40,14 +40,7 @@
         <!-- End Google Tag Manager (noscript) -->
         @endif
 
-        <div id="vue-app">
-            <header class="flex items-center shadow bg-white border-b h-24 py-4" role="banner">
-                <div class="container flex items-center max-w-4xl mx-auto px-4 lg:px-8"
-                </div>
-            </header>
 
-         
-        </div>
 
     </body>
 </html>
