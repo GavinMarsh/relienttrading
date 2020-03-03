fe214b385c6e0eb9314937df7db35c2bd1343f69
Sales manager + developer <39089696+GavinMarsh@users.noreply.github.com>
2020-03-03 21:26:48 +0000

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,6 +1,56 @@
 <html lang="en">
     <head>
-       
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="{{ $page->meta_description or $page->siteDescription }}">
+
+        <!-- Open Graph data -->
+        <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ $page->getUrl() }}"/>
+        <meta property="og:description" content="{{ $page->siteDescription }}" />
+        <meta property="og:image" content="/assets/img/meta.png" />
+
+        <!-- Twitter Card data -->
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="@crypto_mars">
+        <meta name="twitter:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}">
+        <meta name="twitter:description" content="{{ $page->siteDescription }}">
+        <meta name="twitter:creator" content="@crypto_mars">
+        <meta name="twitter:image" content="/assets/img/meta.png">
+
+        <title>
+            @if ( strpos($page->getPath(), 'notes') )
+                @stack('title')
+            @else
+            {{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}
+            @endif
+        </title>
+
+        <link rel="home" href="{{ $page->baseUrl }}">
+        <link rel="icon" href="/favicon.ico">
+        <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
+
+        @stack('meta')
+
+        @if ($page->production)
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89589431-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-89589431-1', { 'optimize_id': 'GTM-M7SGBX9'});
+            </script>
+
+
+
+        @endif
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
 
     <body class="flex flex-col justify-between min-h-screen bg-grey-lightest text-grey-darkest leading-normal font-sans">
